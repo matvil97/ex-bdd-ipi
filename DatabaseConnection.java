8ad6176f4e7b8804b43a753adf7ce331ab3adb46
matvil97 <84513556+matvil97@users.noreply.github.com>
2023-04-13 15:20:56 +0200

    creation classe java + interface + Dao

---
+++ DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private final String url = "jdbc:mysql://localhost:8888/GreenScape";
+    private final String user = "root";
+    private final String password = "1234";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
